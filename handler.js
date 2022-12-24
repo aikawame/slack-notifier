df706f61dd79bf53971558e570b6f355cbb708b1
Masahiro Aikawa <aikawame@wor.jp>
2022-12-24 11:45:36 +0900

    :sparkles: Sentryの通知を追加する

--- handler.js
+++ handler.js
@@ -1,18 +1,49 @@
-'use strict';
+'use strict'
 
-module.exports.hello = async (event) => {
+const { IncomingWebhook } = require('@slack/webhook')
+
+module.exports.sentry = async (event) => {
+  const webhook = new IncomingWebhook(process.env.SLACK_WEBHOOK_URL)
+
+  const sentryBodyStr = event.body
+  const sentryBody = JSON.parse(sentryBodyStr)
+  const sentryEvent = sentryBody.event
+
+  const project = sentryBody.project_slug
+  const title = sentryEvent.title
+  const url = sentryBody.url
+  const culprit = sentryBody.culprit
+
+  const payload = {
+    blocks: [
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `*<${url}|[${project}] ${title}>*`
+        }
+      },
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `${culprit}`
+        }
+      }
+    ]
+  }
+
+  await webhook.send(payload)
+
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+        message: 'OK',
+        input: event
       },
       null,
       2
-    ),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+    )
+  }
+}
